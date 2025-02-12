d025eb6e428caf0665eaa59edaa18779d2b8ff91
Mitchell <mitchell.macdonald10@gmail.com>
2025-02-12 14:17:48 -0800

    Add admin page with user management features and SQLite integration

--- templates/header.php
+++ templates/header.php
@@ -2,19 +2,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <title>The Blog</title>
 </head>
 <header>
+    <div class="logo">
     <h1>THE BLOG</h1>
+    </div>
     <nav>
         <ul class="nav-links">
             <li><a href="../index.php">Articles</a></li>
-            <li><a href="../index.php">Login/Register</a></li>
+            <li><a href="../index.php">Profile</a></li>
+            <li><a href="../index.php">Log Out</a></li>
+            <li><a href="admin/admin_page.php">Admin page (for testing)</a></li>
         </ul>
    
     </nav>
-  
+
 </header>
